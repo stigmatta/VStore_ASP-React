f64edbac95205001c32cf1810f7f914aaef415e7
odintsov-andrey <odintsov.andrey.work@gmail.com>
2025-04-05 13:08:08 +0300

    see in shop slider responsive

--- src/components/SeeInShopSlider.js
+++ src/components/SeeInShopSlider.js
@@ -1,28 +1,28 @@
-import SliderOneGame from "./SliderOneGame";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import SeeInShopGame from "./SeeInShopGame";
 
-export default function SliderGames({ games }) {
+export default function SeeInShopSlider({ games }) {
   const settings = {
     dots: false,
     infinite: true,
-    slidesToShow: 6,
-    slidesToScroll: 2,
+    slidesToShow: 3,
+    slidesToScroll: 1,
     adaptiveHeight: true,
     arrows: false,
   };
 
   return (
     <div className="overflow-x-hidden">
+
       <Slider {...settings}>
         {games.map((game, index) => (
           <div key={index} className="flex-none">
-            <SliderOneGame
+            <SeeInShopGame
               title={game.title}
+              description={game.description}
               image={game.image}
-              price={game.price}
-              discount={game.discount}
             />
           </div>
         ))}
--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,7 +1,11 @@
 import MainGame from "../components/MainGame";
 import TheEndOfTheSun from "../images/the-end-of-the-sun.png"
+import CowboyBebop from '../images/cowboy.png'
 import CategoryTitle from "../components/CategoryTitle";
 import SliderGames from "../components/SliderGames";
+import SeeInShopSlider from "../components/SeeInShopSlider";
+
+
 
 export default function MainPage() {
     const gamesArr = Array(6).fill({
@@ -9,12 +13,20 @@ export default function MainPage() {
       image: TheEndOfTheSun,
       price: 515,
     });
+
+    const seeInShopArr = Array(3).fill({
+        title:"Fortnite",
+        image:CowboyBebop,
+        description:"BANG! Take your shot with bounty hunters Spike Spiegel and Faye Valentine from COWBOY BEBOP.",
+    });
+
     return (
         <div>
             <MainGame title="APEX LEGENDS" subTitle="NEW SEASON" />
 
             <CategoryTitle title="Discover something new"/>
             <SliderGames games = {gamesArr}/>
+            <SeeInShopSlider games={seeInShopArr}/>
 
             <CategoryTitle title="Popular Games"/>
             <SliderGames games = {gamesArr}/>
@@ -24,6 +36,7 @@ export default function MainPage() {
 
             <CategoryTitle title="Now on the store"/>
             <SliderGames games = {gamesArr}/>
+
         </div>
     )
 }
